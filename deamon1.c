251e2cd421d0e654cab11f60d783ad12195596f6
michalkossakowski <kossakowskimichal2002@gmail.com>
2024-03-22 20:06:16 +0100

    testy

--- deamon1.c
+++ deamon1.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() 
+int main(int argCount, char *arg[]) 
 {
-   printf("Hello, World! \n");
+   printf("ilość arg: %d \n",argCount);
+
+   int x = atoi(arg[1]);
+   printf("argument liczba 1: %d \n",x);
    
+   char *y = arg[2];
+   printf("argument string 2: %s \n",y);
    return 0;
 }
